b5a30d15713df6fd47dc514150f0074e7e73ebcb
Riley Fischer <rfmineguy@gmail.com>
2022-09-05 10:50:23 -0700

    LoadTextureEx added

--- include/Resource/Texture.h
+++ include/Resource/Texture.h
@@ -1,5 +1,6 @@
 #ifndef FL_RES_TEXTURE_H
 #define FL_RES_TEXTURE_H
+#include "glad/glad.h"
 
 typedef struct Texture {
     char name[50];
@@ -11,6 +12,7 @@ typedef struct Texture {
 // #define AUTO_RESOURCE_REGISTER for automatic inclusion of individually loaded resources
 
 Texture* LoadTexture(const char*);
+Texture* LoadTextureEx(const char*, GLint, GLint);
 void FreeTexture(Texture*);
 
 void BindTexture(Texture*);

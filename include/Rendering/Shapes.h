1bb57976257f881b08411290b707e6e93845c8fe
Riley Fischer <rfmineguy@gmail.com>
2022-09-07 10:07:22 -0700

    Added ability to render a cube with various transformation options as arguments

--- include/Rendering/Shapes.h
+++ include/Rendering/Shapes.h
@@ -1,11 +1,19 @@
 #ifndef FL_REND_SHAPES_H
 #define FL_REND_SHAPES_H
 #include "../Core/Vec.h"
+#include "../Core/Camera.h"
 
 void InitShapes();
 void FreeShapes();
 
+void BeginDraw(Camera*);
+void EndDraw();
+
 void DrawQuad();
+
+void DrawCube();
+void DrawCubeEx(vec3, vec3, vec3, float, Camera*);
+
 void DrawCircle(Vec2f, float);
 void DrawPoly(Vec2f*, int);
 void DrawLine(Vec2f, Vec2f);
--- src/Shapes.c
+++ src/Shapes.c
@@ -3,12 +3,15 @@
 #include "../include/Resource/ResourceManager.h"
 #include <glad/glad.h>
 
-static Geometry quad;
+static Geometry quad, cube;
+static Shader* camShader;
 
 void InitShapes() {
     static bool init = false;
     if (!init) {
         PopulateQuad(&quad);
+        PopulateCube(&cube);
+        camShader = LoadShader("res/shader/", "camera");
         init = true;
     }
 }
@@ -17,6 +20,8 @@ void FreeShapes() {
     static bool free = false;
     if (!free) {
         FreeGeometry(&quad);
+        FreeGeometry(&cube);
+        FreeShader(camShader);
         free = true;
     }
 }
@@ -24,4 +29,27 @@ void FreeShapes() {
 void DrawQuad() {
     BindGeometry(&quad);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+void DrawCube() {
+    BindGeometry(&cube);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+}
+
+void DrawCubeEx(vec3 pos, vec3 scale, vec3 rotAxis, float angle, Camera* pCamera) {
+    BindShader(camShader);
+    
+    mat4 transform;
+    glm_mat4_identity(transform);
+    glm_translate(transform, pos);
+    glm_scale(transform, scale);
+    glm_rotate(transform, glm_rad(angle), rotAxis);
+
+    SetUniform4fv(camShader, "view", pCamera->view[0]);
+    SetUniform4fv(camShader, "projection", pCamera->proj[0]);
+
+    SetUniform4fv(camShader, "model", transform[0]);
+
+    BindGeometry(&cube);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 }

e0e9b20089dfcd81ff9deef59982eae869b60851
rfmineguy <rfmineguy@gmail.com>
2022-09-28 19:30:50 -0700

    Apparently I forgot to include Log.c in the logging commit

--- src/Log.c
+++ src/Log.c
@@ -3,7 +3,13 @@
 #include <string.h>
 #include <stdarg.h>
 
-#define TEXT_BLACK 
+#define LOG_DEBUG_COLOR_CYAN     "\x1B[36m"
+#define LOG_INFO_COLOR_GREEN     "\x1B[32m"
+#define LOG_WARN_COLOR_YELLOW    "\x1B[33m"
+#define LOG_ERROR_COLOR_RED      "\x1B[31m"
+#define LOG_CRITICAL_COLOR_MAG   "\x1B[35m"
+
+#define LOG_RESET_COLOR          "\x1B[0m"
 
 static FILE* activeLogStream = NULL;
 
@@ -20,17 +26,35 @@ void LogFL(LogLevel lvl, char* file, int line, const char* msg, ...) {
     char buffer[500] = {};
     char msgbuffer[255] = {};
     char levelStr[12] = {};
-    if (lvl == DEBUG)    strcat(levelStr, "DEBUG");
-    if (lvl == INFO)     strcat(levelStr, "INFO");
-    if (lvl == WARN)     strcat(levelStr, "WARN");
-    if (lvl == ERROR)    strcat(levelStr, "ERROR");
-    if (lvl == CRITICAL) strcat(levelStr, "CRIT");
-    
+    char color[9] = {};
+    switch (lvl) {
+    case DEBUG:
+        strcat(color, LOG_DEBUG_COLOR_CYAN);
+        strcat(levelStr, "DEBUG");
+        break;
+    case INFO:
+        strcat(color, LOG_INFO_COLOR_GREEN);
+        strcat(levelStr, "INFO");
+        break;
+    case WARN:
+        strcat(color, LOG_WARN_COLOR_YELLOW);
+        strcat(levelStr, "WARN");
+        break;
+    case ERROR:
+        strcat(color, LOG_ERROR_COLOR_RED);
+        strcat(levelStr, "ERROR");
+        break;
+    case CRITICAL:
+        strcat(color, LOG_CRITICAL_COLOR_MAG);
+        strcat(levelStr, "CRIT");
+        break;
+    }    
+
     va_list args;
     va_start(args, msg);
     vsprintf(msgbuffer, msg, args);
     va_end(args);
 
-    sprintf(buffer, "[%s, %s, %d]: %s", levelStr, file, line, msgbuffer);
+    sprintf(buffer, "%s[%s, %s, %d]: %s%s", color, levelStr, file, line, msgbuffer, LOG_RESET_COLOR);
     fprintf(activeLogStream, "%s\n", buffer);
 }

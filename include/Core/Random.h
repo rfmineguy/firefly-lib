4cd32e375a72c7b04dd055904b8a226adb78a389
rfmineguy <rfmineguy@gmail.com>
2022-10-11 14:04:54 -0700

    Added the ability to choose a random number from a set/array of doubles

--- include/Core/Random.h
+++ include/Core/Random.h
@@ -9,6 +9,7 @@ void FF_RandomSeedLong(uint32_t);
 //not inclusive
 int FF_RandomRangeInt(int, int);
 int FF_RandomInt();
+double FF_RandomFromSet(int, double*);
 
 float FF_RandomRangeFloat(float, float);
 float FF_RandomFloat();
--- src/Random.c
+++ src/Random.c
@@ -1,5 +1,10 @@
 #include "../include/Core/Random.h"
+#include "../include/IO/IO.h"
 #include <time.h>
+#include <stdarg.h>
+
+#define FF_RandomSet(...) __FF_RandomSet()
+#define ARRAY_LEN(x) (sizeof(x) / sizeof(x[0]))
 
 void FF_RandomSeedTime() {
   srand(time(NULL));
@@ -10,12 +15,19 @@ void FF_RandomSeedLong(uint32_t seed) {
 }
 
 int FF_RandomRangeInt(int lower, int upper) {
+  // [0, upper - lower] => [lower, upper]
   uint32_t r = arc4random_uniform(upper - lower);
   return r + lower;
 }
 
 int FF_RandomInt() {
+  // [0-1]
   return FF_RandomRangeInt(0, 1);
+}
+
+double FF_RandomFromSet(int size, double *arr) {
+  int index = FF_RandomRangeInt(0, size);
+  return arr[index];
 }
 
 float FF_RandomRangeFloat(float lower, float upper) {

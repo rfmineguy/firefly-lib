9947427b73d28da6f32cacb696ba7be35a6d57ee
rfmineguy <rfmineguy@gmail.com>
2022-10-12 10:21:38 -0700

    Added linear interpolation, and a broken smoothstep interpolation

--- include/Core/Interpolation.h
+++ include/Core/Interpolation.h
@@ -1,6 +1,11 @@
 #ifndef INTERPOLATION_H
 #define INTERPOLATION_H
 
+typedef enum {
+  FF_LINEAR, FF_SMOOTHSTEP
+} LerpFunction;
+
 double FF_Lerp(double, double, double);
+double FF_LerpFunc(LerpFunction func, double a, double b, double t);
 
 #endif
--- src/Interpolation.c
+++ src/Interpolation.c
@@ -1,5 +1,29 @@
 #include "../include/Core/Interpolation.h"
+#include <math.h>
+
+double Clamp(double x, double min, double max) {
+  if (x < min) {
+    x = min;
+  }
+  if (x > max) {
+    x = max;
+  }
+  return x;
+}
 
 double FF_Lerp(double a, double b, double t) {
   return b * t + a * (1 - t);  
+}
+
+double FF_LerpFunc(LerpFunction func, double a, double b, double t) {
+  switch (func) {
+  case FF_LINEAR: 
+      return FF_Lerp(a, b, t);// b * t + a * (1 - t);
+  case FF_SMOOTHSTEP: 
+    {
+        t = Clamp((t - a) / (a / b), 0.0f, 1.0f);
+        return t * t * (3 - 2 * t);
+    }
+  default: return 0;
+  }
 }

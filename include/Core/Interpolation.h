9395ed25e52de0dce3f503e940b8670f524a8c97
rfmineguy <rfmineguy@gmail.com>
2022-10-18 23:15:03 -0700

    Added interpolation Function pointers

--- include/Core/Interpolation.h
+++ include/Core/Interpolation.h
@@ -5,7 +5,10 @@ typedef enum {
   FF_LINEAR, FF_SMOOTHSTEP
 } LerpFunction;
 
-double FF_Lerp(double, double, double);
-double FF_LerpFunc(LerpFunction func, double a, double b, double t);
+double FF_LerpFuncLinear(double, double, double);
+double FF_LerpFuncLinearLog(double, double, double);
+double FF_LerpFuncSmoothstep(double, double, double);
+double FF_LerpFuncSlerp(double, double, double);
+double FF_LerpFunc(double(*)(double, double, double), double, double, double);
 
 #endif
--- src/Interpolation.c
+++ src/Interpolation.c
@@ -11,19 +11,22 @@ double Clamp(double x, double min, double max) {
   return x;
 }
 
-double FF_Lerp(double a, double b, double t) {
+double FF_LerpFuncLinear(double a, double b, double t) {
   return b * t + a * (1 - t);  
 }
 
-double FF_LerpFunc(LerpFunction func, double a, double b, double t) {
-  switch (func) {
-  case FF_LINEAR: 
-      return FF_Lerp(a, b, t);// b * t + a * (1 - t);
-  case FF_SMOOTHSTEP: 
-    {
-        t = Clamp((t - a) / (a / b), 0.0f, 1.0f);
-        return t * t * (3 - 2 * t);
-    }
-  default: return 0;
-  }
+double FF_LerpFuncLinearLog(double a, double b, double t) {
+  return log(b) * t + log(a) * (1 - t);  
+}
+
+double FF_LerpFuncSmoothstep(double a, double b, double t) {
+  return 0;
+}
+
+double FF_LerpFuncSlerp(double a, double b, double t) {
+  return 0;
+}
+
+double FF_LerpFunc(double(*func)(double, double, double), double a, double b, double t) {
+  return func(a, b, t);
 }
